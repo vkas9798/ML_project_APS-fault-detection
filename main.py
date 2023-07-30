77689973efd89097e337697cd7f6584d8c699ec8
vkas9798 <sharmavikas.9798@gmail.com>
2023-07-30 23:30:27 +0530

    logger tested for sample case error in main.py

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
-import pymongo
-import pandas as pd
+from sensor.logger import logging
+from sensor.exception import SensorException
+import sys,os
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/")
+def test_logger_and_exception():
+     try:
+          logging.info("Starting the test_logger_and_exception")
+          result = 3/0
+          print(result)
+          logging.info("Stopping the test_logger_and_exception")
+     except Exception as e:
+          logging.debug(str(e))
+          raise SensorException(e,sys)
 
-# Database Name
-dataBase = client["labDB"]
+if __name__=="__main__":
+     try:
+          test_logger_and_exception()
+     except Exception as e:
+          print(e)
 
-# Collection  Name
-collection = dataBase['Products']
-
-# Sample data
-d = {'companyName': '----',
-     'product': '----',
-     'Offered': 'Machine Learning with Deployment'}
-
-# Insert above records in the collection
-rec = collection.insert_one(d)
-
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
-
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
